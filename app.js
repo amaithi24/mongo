c638f1efe47b3356399d1c27b4ca9a42fc1efbbc
amaithi24 <59793035+amaithi24@users.noreply.github.com>
2020-01-13 19:55:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = "mongodb://amaithi24:B97nL8324ZXZAm3hu7Q8rGxKSUzQmeh78KIiwrzHE6dxJLH7XzNJMvtWrxYMTWYav2MvazK5bit1LwefI0DoBQ%3D%3D@amaithi24.documents.azure.com:10255/?ssl=true";
-const express = require('express');
-const bodyParser = require('body-parser');
-const stringify = require('json-stringify-safe')
-// create express app
-const app = express();
 
-// parse requests of content-type - application/x-www-form-urlencoded
+
+
 app.use(bodyParser.urlencoded({ extended: true }))
-
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("familiesdb");
+
+
+
+
 app.get('/', (req, res) => {
-    dbo.collection("families").findOne({}, function(err, result) {
+
+    MongoClient.connect(url, function(err, db) {
+        console.log("sds");
         if (err) throw err;
-        console.log(result);
-        res.send(result)
-        db.close();
-      });
+        var dbo = db.db("familiesdb");
+        dbo.collection("families").findOne({}, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+           // res.send(result)
+            //db.closeConnection();
+          });
+        });
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!jjjjj');
 });
 
- 
-});
-app.listen(8081, () => {
+app.listen(8080, () => {
     console.log("Server is listening on port 8080");
 });
